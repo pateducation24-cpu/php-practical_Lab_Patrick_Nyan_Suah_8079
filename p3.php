cf386bde22fe40f5b71bd53841549471af2ded0a
pateducation24-cpu <pat.education24@gmail.com>
2026-06-01 12:58:17 +0530

    Add files via upload

--- p3.php
+++ p3.php
@@ -3,8 +3,8 @@
 $a = 50;
 $b = 20;
 
-echo "a = ".$a;
-echo "b = ".$b."<br>"."<br>;
+echo "a = ".$a."<br>";
+echo "b = ".$b."<br>"."<br>";
 
 $c= $a+$b;
 echo "Additon:".$c."<br>";
@@ -12,10 +12,29 @@ echo "Additon:".$c."<br>";
 $d = $a*$b;
 echo "Multiplication:".$d."<br>";
 $e = $a**$b;
-echo "50 raised to 20 is:".$e."<br>";
+
+$div = $a/$b;
+echo "Division:".$div."<br>";
+
+$sub =$a-$b;
+echo "Subtraction:".$sub."<br>";
+
+if ($a == $b){
+    echo "a = b";
+   } else{
+        echo "Equality:both are not equla"."<br>";
+    }
+
+
+
+
+$unary_operator = $a++;
+echo "Unary Operation:".$unary_operator."<br>";
+
+echo "Exponentiation:".$e."<br>";
 
 $f = $b%$a;
-echo "Remainder of 50 divide by 20 is:".$f."<br>";
+echo "Modulus Operation:".$f."<br>";
 
 
 
