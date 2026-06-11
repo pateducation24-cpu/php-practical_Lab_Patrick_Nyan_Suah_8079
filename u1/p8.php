bddcb6a1c50be97c7e1d4d180ad2e2987571720c
pateducation24-cpu <pat.education24@gmail.com>
2026-06-11 10:04:22 +0530

    Update p8.php

--- u1/p8.php
+++ u1/p8.php
@@ -4,16 +4,13 @@ echo "<h3> Entered Values:</h3>";
 foreach($nm as $val){
     echo $val."<br>";
 }
-
 ?>
-    <form method="post">
+    <form action ="" method="post">
         <h3> Enter Names </h3>
         <input type="text" name ="uname[0]"> <br>
         <input type="text" name ="uname[1]"> <br>
         <input type="text" name ="uname[2]"> <br>
+        <input type ="submit" value ="Add">
         
     </form>
-<?php
-}
-?>
 
