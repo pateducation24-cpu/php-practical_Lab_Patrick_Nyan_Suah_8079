4409b7fca8008a256c9a232666c09dd2cff015d3
pateducation24-cpu <pat.education24@gmail.com>
2026-08-23 21:13:22 +0530

    Add files via upload

---
+++ p1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JavaScript Alert Example</title>
+</head>
+<body>
+    <h2>JavaScript Alert Example</h2>
+    <button onclick="showAlert()">Click Me</button>
+
+    <script>
+        function showAlert() {
+            var message = "Hello! Welcome to JavaScript!";
+            alert(message);
+        }
+    </script>
+</body>
+</html>
